a43a34ec22b3f633f7c68be54c3d874b619399ce
TangleSpace <c6lculus8ntr0py@gmail.com>
2021-07-19 09:01:26 +1000

    updated heroku

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ echo "\
 [general]\n\
 email = \"{c6lculus8ntr0py@gmail.com}\"\n\
 " > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\port = $PORT\n\
+" > ~/.streamlit/config.toml
